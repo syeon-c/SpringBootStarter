3d586bf29577f4e88dbe5ff6932b00fa7afd7d6b
최서연 <gkdltjdus@gmail.com>
2021-03-20 15:15:49 +0900

    #1 View Environment Settings

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!");
-        return "hello";
+        model.addAttribute("data", "hello!!");
+        return "hello";     // resources/templates/hello.html 파일 찾아서 반환
     }
 }
